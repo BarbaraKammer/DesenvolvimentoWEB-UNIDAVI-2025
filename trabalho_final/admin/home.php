83a24dd14f9b868b09ee3d6db126f14c432ed795
BarbaraKammer <barbarackammer@gmail.com>
2025-12-01 11:41:39 -0300

    Melhorando usabilidade do painel admin

--- trabalho_final/admin/home.php
+++ trabalho_final/admin/home.php
@@ -2,18 +2,43 @@
 <h2 class="mb-4">Bem-vindo ao Painel Administrativo</h2>
 
 <div class="row">
+
   <div class="col-md-6">
-    <div class="card"><h4>📊 Avaliações</h4><p>Acompanhe os resultados e métricas.</p></div>
+    <a href="dashboard.php" class="text-decoration-none">
+      <div class="card card-click">
+        <h4>📊 Gráficos</h4>
+        <p>Acompanhe os resultados e métricas.</p>
+      </div>
+    </a>
   </div>
+
   <div class="col-md-6">
-    <div class="card"><h4>⚙️ Perguntas</h4><p>Gerencie perguntas do formulário.</p></div>
+    <a href="perguntas.php" class="text-decoration-none">
+      <div class="card card-click">
+        <h4>⚙️ Perguntas</h4>
+        <p>Gerencie perguntas do formulário.</p>
+      </div>
+    </a>
   </div>
+
   <div class="col-md-6 mt-3">
-    <div class="card"><h4>📱 Dispositivos</h4><p>Controle os tablets de avaliação.</p></div>
+    <a href="dispositivos.php" class="text-decoration-none">
+      <div class="card card-click">
+        <h4>📱 Dispositivos</h4>
+        <p>Controle os tablets de avaliação.</p>
+      </div>
+    </a>
   </div>
+
   <div class="col-md-6 mt-3">
-    <div class="card"><h4>🏢 Setores</h4><p>Organize e monitore os setores de atendimento.</p></div>
+    <a href="setores.php" class="text-decoration-none">
+      <div class="card card-click">
+        <h4>🏢 Setores</h4>
+        <p>Organize e monitore os setores de atendimento.</p>
+      </div>
+    </a>
   </div>
+
 </div>
 
 <?php include "footer.php"; ?>
